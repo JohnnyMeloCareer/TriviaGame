764a31dea1689f318d794d51834f10733ad7f6fe
Johnny Melo <Johnnymelowork@gmail.com>
2018-11-19 19:13:40 -0500

    FirstPush

---
+++ assets/JavaScript/app.js
@@ -0,0 +1,67 @@
+// Array of object questions
+var questions = [
+  {
+      prompt: "What was the name of the final boss from Splatoon 2?\n(a) Octobot King I\n\
+               (b) Octobot King II\n(c) DJ Octavio",
+      answer: "b"
+  },
+{
+    prompt: "How many Splatlings are in the Splatling category/family?\n(a) 9\n\
+             (b) 8\n(c) 7",
+      answer: "a"
+},
+{
+    prompt: "What map is loved by most charger mains?\n(a) Walleye Ware\n\
+    (b) Skipper Pavilion\n(c) Camp triggerfish",
+      answer: "a"
+},
+{
+    prompt: "Who the final boss in the Octo Expansion DLC?"\n(a) Agent 3\n\
+    (b) Telephone\n(c) Marina",
+      answer: "b"
+},
+{
+    prompt: "What the maximum level one can achieve?"\n(a) 99\n\
+    (b) 99*\n(c) 99*^99",
+      answer: "c"
+},
+{
+    prompt: "What was the max rank before rank x was released?"\n(a) 99\n\
+    (b) 10\n(c) 50",
+      answer: "c"
+},
+{
+    prompt: "How many sub chunks equals a main?"\n(a) 3\n\
+    (b) 2\n(c) 4",
+      answer: "a"
+},
+{
+    prompt: "How many maps are there as of date 11/18/2018"\n(a) 22\n\
+    (b) 23\n(c) 21",
+      answer: "b"
+},
+{
+    prompt: "When was Splatoon 1 released?"\n(a) May28th2015\n\
+    (b) May27th2015\n(c) May26th2015",
+      answer: "a"
+},
+{
+    prompt: "When was Splatoon 2 released?"\n(a) July21th2018\n\
+    (b) July22th2018\n(c) July23th2018",
+      answer: "a"
+},
+{
+    prompt: "What weapon require's no skills?"\n(a) Clash Blaster\n\
+    (b) Luna Blaster\n(c) Blaster",
+      answer: "a"
+},
+{
+    prompt: "Predict the year Splatoon 3 will come out."\n(a) 2020\n\
+    (b) 2021\n(c) 2022",
+      answer: "b"
+},
+
+var questions;
+for (i = 0; i < questions.length; i++) { 
+
+}
